1ee471a6fc305d9a0cbdea14adb57278d8300ce1
qarap <qaraptapqan@gmail.com>
2025-01-31 19:27:29 +0500

    lab2

---
+++ lab2/_4.py
@@ -0,0 +1,11 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+print(thislist[-1])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+print(thislist[:4])
+
+print(thislist[2:])
